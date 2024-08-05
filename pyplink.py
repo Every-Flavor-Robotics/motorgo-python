4de82fd2656bcd918c9476d0d7a28298ec5558c6
Swapnil Pande <swapnil.pande98@gmail.com>
2024-08-05 16:12:18 -0400

    Add comments and correctly close SPI connection on shutdown

--- pyplink.py
+++ pyplink.py
@@ -2,6 +2,7 @@
 import time
 import struct
 import threading
+import atexit
 
 # Enums to match C++ definitions
 class ControlMode:
@@ -12,10 +13,11 @@ class BrakeMode:
     BRAKE = 0
     COAST = 1
 
-test = 0
-
 class MotorChannel:
     def __init__(self):
+        """
+        Initialize the MotorChannel with default control and brake modes.
+        """
         self.lock = threading.Lock()
 
         self.control_mode = ControlMode.POWER
@@ -28,150 +30,118 @@ class MotorChannel:
         self._position = 0.0
         self._velocity = 0.0
 
-
-
     @property
-    def velocity_command(self):
-        # Acquire lock
-        self.lock.acquire()
-        # Get the value
-        value = self._velocity_command
-        # Release lock
-        self.lock.release()
-
-        return value
+    def velocity_command(self) -> float:
+        """
+        Get the velocity command with thread-safe access.
+        """
+        with self.lock:
+            return self._velocity_command
 
     @velocity_command.setter
-    def velocity_command(self, value):
-        # Acquire lock
-        self.lock.acquire()
-
-        if self._control_mode != ControlMode.VELOCITY:
-            # Return a warning if the control mode is not set to velocity
-            print("Warning: Setting velocity command with control mode set to power.")
-
-        # Set the value
-        self._velocity_command = value
-        # Release lock
-        self.lock.release()
+    def velocity_command(self, value: float):
+        """
+        Set the velocity command with thread-safe access and mode check.
+        """
+        with self.lock:
+            if self._control_mode != ControlMode.VELOCITY:
+                print("Warning: Setting velocity command with control mode set to power.")
+            self._velocity_command = value
 
     @property
-    def power_command(self):
-        # Acquire lock
-        self.lock.acquire()
-        # Get the value
-        value = self._power_command
-        # Release lock
-        self.lock.release()
-
-        return value
+    def power_command(self) -> float:
+        """
+        Get the power command with thread-safe access.
+        """
+        with self.lock:
+            return self._power_command
 
     @power_command.setter
-    def power_command(self, value):
-        # Acquire lock
-        self.lock.acquire()
-
-        if self._control_mode != ControlMode.POWER:
-            # Return a warning if the control mode is not set to power
-            print("Warning: Setting power command with control mode set to velocity.")
-
-        # Set the value
-        self._power_command = value
-        # Release lock
-        self.lock.release()
+    def power_command(self, value: float):
+        """
+        Set the power command with thread-safe access and mode check.
+        """
+        with self.lock:
+            if self._control_mode != ControlMode.POWER:
+                print("Warning: Setting power command with control mode set to velocity.")
+            self._power_command = value
 
     @property
-    def control_mode(self):
-        # Acquire lock
-        self.lock.acquire()
-        # Get the value
-        value = self._control_mode
-        # Release lock
-        self.lock.release()
-
-        return value
+    def control_mode(self) -> int:
+        """
+        Get the control mode with thread-safe access.
+        """
+        with self.lock:
+            return self._control_mode
 
     @control_mode.setter
-    def control_mode(self, value):
-        # Acquire lock
-        self.lock.acquire()
-        # Set the value
-        self._control_mode = value
-        # Release lock
-        self.lock.release()
+    def control_mode(self, value: int):
+        """
+        Set the control mode with thread-safe access.
+        """
+        with self.lock:
+            self._control_mode = value
 
     @property
-    def brake_mode(self):
-        # Acquire lock
-        self.lock.acquire()
-        # Get the value
-        value = self._brake_mode
-        # Release lock
-        self.lock.release()
-
-        return value
+    def brake_mode(self) -> int:
+        """
+        Get the brake mode with thread-safe access.
+        """
+        with self.lock:
+            return self._brake_mode
 
     @brake_mode.setter
-    def brake_mode(self, value):
-        # Acquire lock
-        self.lock.acquire()
-        # Set the value
-        self._brake_mode = value
-        # Release lock
-        self.lock.release()
+    def brake_mode(self, value: int):
+        """
+        Set the brake mode with thread-safe access.
+        """
+        with self.lock:
+            self._brake_mode = value
 
     @property
-    def command(self):
-        if self.control_mode == ControlMode.VELOCITY:
-            return self.velocity_command
-        else:
-            return self.power_command
+    def command(self) -> float:
+        """
+        Get the command based on the current control mode.
+        """
+        return self.velocity_command if self.control_mode == ControlMode.VELOCITY else self.power_command
 
     @property
-    def position(self):
-        # Acquire lock
-        self.lock.acquire()
-        # Get the value
-        value = self._position
-        # Release lock
-        self.lock.release()
+    def position(self) -> float:
+        """
+        Get the position with thread-safe access.
+        """
+        with self.lock:
+            return self._position
 
-        return value
-
-    def update_position(self, value):
-        # Acquire lock
-        self.lock.acquire()
-        # Set the value
-        self._position = value
-        # Release lock
-        self.lock.release()
+    def update_position(self, value: float):
+        """
+        Update the position with thread-safe access.
+        """
+        with self.lock:
+            self._position = value
 
     @property
-    def velocity(self):
-        # Acquire lock
-        self.lock.acquire()
-        # Get the value
-        value = self._velocity
-        # Release lock
-        self.lock.release()
+    def velocity(self) -> float:
+        """
+        Get the velocity with thread-safe access.
+        """
+        with self.lock:
+            return self._velocity
 
-        return value
-
-
-    def update_velocity(self, value):
-        # Acquire lock
-        self.lock.acquire()
-        # Set the value
-        self._velocity = value
-        # Release lock
-        self.lock.release()
-
-
+    def update_velocity(self, value: float):
+        """
+        Update the velocity with thread-safe access.
+        """
+        with self.lock:
+            self._velocity = value
 
 class OutputStruct:
     BUFFER_OUT_SIZE = 49
-    def __init__(self, channel1: MotorChannel, channel2: MotorChannel, channel3: MotorChannel, channel4: MotorChannel):
 
+    def __init__(self, channel1: MotorChannel, channel2: MotorChannel, channel3: MotorChannel, channel4: MotorChannel):
+        """
+        Initialize the output structure with motor channels' commands and modes.
+        """
         self.valid = False
 
         self.channel_1_command = channel1.command
@@ -189,48 +159,58 @@ class OutputStruct:
         self.channel_3_brake_mode = channel3.brake_mode
         self.channel_4_brake_mode = channel4.brake_mode
 
-    def get_packed_struct(self):
+    def get_packed_struct(self) -> bytes:
+        """
+        Pack the structure data into bytes for transmission.
+        """
         return struct.pack('4f8B',
-        self.channel_1_command,
-        self.channel_2_command,
-        self.channel_3_command,
-        self.channel_4_command,
-        self.channel_1_control_mode,
-        self.channel_2_control_mode,
-        self.channel_3_control_mode,
-        self.channel_4_control_mode,
-        self.channel_1_brake_mode,
-        self.channel_2_brake_mode,
-        self.channel_3_brake_mode,
-        self.channel_4_brake_mode)
+                           self.channel_1_command,
+                           self.channel_2_command,
+                           self.channel_3_command,
+                           self.channel_4_command,
+                           self.channel_1_control_mode,
+                           self.channel_2_control_mode,
+                           self.channel_3_control_mode,
+                           self.channel_4_control_mode,
+                           self.channel_1_brake_mode,
+                           self.channel_2_brake_mode,
+                           self.channel_3_brake_mode,
+                           self.channel_4_brake_mode)
 
-    def __str__(self):
-
-        return "OutputStruct:\n" + \
-            f"Valid: {self.valid}\n" + \
-            f"Channel 1 Command: {self.channel_1_command}\n" + \
-            f"Channel 2 Command: {self.channel_2_command}\n" + \
-            f"Channel 3 Command: {self.channel_3_command}\n" + \
-            f"Channel 4 Command: {self.channel_4_command}\n" + \
-            f"Channel 1 Control Mode: {self.channel_1_control_mode}\n" + \
-            f"Channel 2 Control Mode: {self.channel_2_control_mode}\n" + \
-            f"Channel 3 Control Mode: {self.channel_3_control_mode}\n" + \
-            f"Channel 4 Control Mode: {self.channel_4_control_mode}\n" + \
-            f"Channel 1 Brake Mode: {self.channel_1_brake_mode}\n" + \
-            f"Channel 2 Brake Mode: {self.channel_2_brake_mode}\n" + \
-            f"Channel 3 Brake Mode: {self.channel_3_brake_mode}\n" + \
-            f"Channel 4 Brake Mode: {self.channel_4_brake_mode}\n"
+    def __str__(self) -> str:
+        """
+        Return a string representation of the output structure.
+        """
+        return (f"OutputStruct:\n"
+                f"Valid: {self.valid}\n"
+                f"Channel 1 Command: {self.channel_1_command}\n"
+                f"Channel 2 Command: {self.channel_2_command}\n"
+                f"Channel 3 Command: {self.channel_3_command}\n"
+                f"Channel 4 Command: {self.channel_4_command}\n"
+                f"Channel 1 Control Mode: {self.channel_1_control_mode}\n"
+                f"Channel 2 Control Mode: {self.channel_2_control_mode}\n"
+                f"Channel 3 Control Mode: {self.channel_3_control_mode}\n"
+                f"Channel 4 Control Mode: {self.channel_4_control_mode}\n"
+                f"Channel 1 Brake Mode: {self.channel_1_brake_mode}\n"
+                f"Channel 2 Brake Mode: {self.channel_2_brake_mode}\n"
+                f"Channel 3 Brake Mode: {self.channel_3_brake_mode}\n"
+                f"Channel 4 Brake Mode: {self.channel_4_brake_mode}\n")
 
 
 class InputStruct:
     BUFFER_IN_SIZE = 33
-    def __init__(self, data = None):
-        if(data is not None):
+
+    def __init__(self, data: bytes = None):
+        """
+        Initialize the input structure and decode data if provided.
+        """
+        if data is not None:
             self.decode(data)
 
-
-    def decode(self, data):
-        # Unpack the data based on the packed struct definition
+    def decode(self, data: bytes):
+        """
+        Decode the input data into the structure fields.
+        """
         unpacked_data = struct.unpack_from('<?8f', data)
         self.valid = unpacked_data[0]
         self.channel_1_pos = unpacked_data[1]
@@ -242,24 +222,27 @@ class InputStruct:
         self.channel_4_pos = unpacked_data[7]
         self.channel_4_vel = unpacked_data[8]
 
-    def __str__(self):
-        return "InputStruct:\n" + \
-            f"Valid: {self.valid}\n" + \
-            f"Channel 1 Position: {self.channel_1_pos}\n" + \
-            f"Channel 1 Velocity: {self.channel_1_vel}\n" + \
-            f"Channel 2 Position: {self.channel_2_pos}\n" + \
-            f"Channel 2 Velocity: {self.channel_2_vel}\n" + \
-            f"Channel 3 Position: {self.channel_3_pos}\n" + \
-            f"Channel 3 Velocity: {self.channel_3_vel}\n" + \
-            f"Channel 4 Position: {self.channel_4_pos}\n" + \
-            f"Channel 4 Velocity: {self.channel_4_vel}\n"
+    def __str__(self) -> str:
+        """
+        Return a string representation of the input structure.
+        """
+        return (f"InputStruct:\n"
+                f"Valid: {self.valid}\n"
+                f"Channel 1 Position: {self.channel_1_pos}\n"
+                f"Channel 1 Velocity: {self.channel_1_vel}\n"
+                f"Channel 2 Position: {self.channel_2_pos}\n"
+                f"Channel 2 Velocity: {self.channel_2_vel}\n"
+                f"Channel 3 Position: {self.channel_3_pos}\n"
+                f"Channel 3 Velocity: {self.channel_3_vel}\n"
+                f"Channel 4 Position: {self.channel_4_pos}\n"
+                f"Channel 4 Velocity: {self.channel_4_vel}\n")
 
 class Plink:
-    BUFFER_IN_SIZE = 49
 
-    def __init__(self, frequency = 100, timeout = 5.0):
-
-        # Configure SPI bus
+    def __init__(self, frequency: int = 100, timeout: float = 5.0):
+        """
+        Initialize the Plink communication object with motor channels and communication settings.
+        """
         # self.spi = spidev.SpiDev()
         # self.spi.open(0, 0)  # Open bus 0, device (CS) 0
         # self.spi.mode = 0
@@ -272,18 +255,28 @@ class Plink:
         self.channel3 = MotorChannel()
         self.channel4 = MotorChannel()
 
-
         self.frequency = frequency
         self.timeout = timeout
 
     def connect(self):
-        # Start thread to send and receive data
+        """
+        Start the communication thread.
+        """
+        atexit.register(self.shutdown)  # Register cleanup function
+
+        self.running = True
         self.thread = threading.Thread(target=self.comms_thread)
+        self.thread.daemon = True  # Set the thread as a daemon thread
         self.thread.start()
 
 
     def update_motor_states(self, response: InputStruct):
+        """
+        Update the motor states based on the input structure data.
 
+        Args:
+            response (InputStruct): The input structure containing the motor states.
+        """
         # Update the motor states
         self.channel1.update_position(response.channel_1_pos)
         self.channel1.update_velocity(response.channel_1_vel)
@@ -298,6 +291,9 @@ class Plink:
         self.channel4.update_velocity(response.channel_4_vel)
 
     def transfer(self):
+        """
+        Prepare and send data, then receive and process the response.
+        """
         # Prepare data from current state
         data = OutputStruct(
             self.channel1,
@@ -306,40 +302,43 @@ class Plink:
             self.channel4)
         data.valid = True
 
+        # Send data and receive response (Mock response for now)
         # response = InputStruct(self.spi.xfer2(data.get_packed_struct()))
-        # Mock response for now
-        # Print the data out
-        # print(data)
 
+        # Mock response for testing
         response = InputStruct()
         response.valid = True
-        global test
-        response.channel_1_pos = 1.0 + test
-        response.channel_1_vel = 2.0 + test
-        response.channel_2_pos = 3.0 + test
-        response.channel_2_vel = 4.0 + test
-        response.channel_3_pos = 5.0 + test
-        response.channel_3_vel = 6.0 + test
-        response.channel_4_pos = 7.0 + test
-        response.channel_4_vel = 8.0 + test
-        test += 1
+
+        response.channel_1_pos = 1.0
+        response.channel_1_vel = 2.0
+        response.channel_2_pos = 3.0
+        response.channel_2_vel = 4.0
+        response.channel_3_pos = 5.0
+        response.channel_3_vel = 6.0
+        response.channel_4_pos = 7.0
+        response.channel_4_vel = 8.0
+
 
         # Update the motor states
-        if(response.valid):
+        if response.valid:
             self.update_motor_states(response)
 
-            if(self.last_message_time == None):
+            if self.last_message_time is None:
                 print("Connection established!")
 
             self.last_message_time = time.time()
 
     def comms_thread(self):
+        """
+        Communication thread to handle periodic data transfer.
+        """
         try:
-            while True:
+            while self.running:
                 start = time.time()
                 # Send the message
                 self.transfer()
 
+                # Check for response timeout
                 if self.last_message_time is not None:
                     if time.time() - self.last_message_time > self.timeout:
                         print("No response from SPI device")
@@ -347,18 +346,23 @@ class Plink:
 
                 # Sleep for the remainder of the cycle
                 delta = time.time() - start
-                sleep_time = 1.0/self.frequency - delta
+                sleep_time = 1.0 / self.frequency - delta
                 time.sleep(sleep_time if sleep_time > 0 else 0)
 
         except KeyboardInterrupt:
             # Handle keyboard interrupt (Ctrl+C)
             self.shutdown()
 
+        finally:
+            # Ensure cleanup code runs when the thread is stopped
+            self.shutdown()
+
     def shutdown(self):
-        # Clean up resources and perform shutdown tasks
+        """
+        Clean up resources and perform shutdown tasks.
+        """
+        self.running = False
+
+        print("Disconnecting from Plink ...")
+        # Close the SPI connection
         # self.spi.close()
-        pass
-
-
-
-
