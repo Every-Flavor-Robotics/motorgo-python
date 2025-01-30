4b64fc5b07ad01515673a92e5d7fd43d8d0840f0
Swapnil Pande <swapnil.pande98@gmail.com>
2025-01-30 15:37:34 -0500

    flash -> flash_utils

--- motorgo/cli/cli.py
+++ motorgo/cli/cli.py
@@ -1,5 +1,5 @@
 import click
-from .flash import download_and_flash_firmware
+from .flash_utils import download_and_flash_firmware
 
 
 @click.group()
@@ -12,13 +12,13 @@ def flash():
     """
     Flash an ESP32-S3 with the latest firmware.
     """
-    click.secho("Setting up MotorGo for PiHat Mode operation", fg="green")
+    click.secho("Configuring MotorGo for PiHat mode", fg="green")
 
     click.secho("Step 1: Identify MotorGo board", fg="blue")
     click.secho("Step 2: Downloading latest firmware", fg="blue")
     download_and_flash_firmware()
 
-    click.secho("MotorGo successfully configured!", fg="green")
+    click.secho("MotorGo successfully configured for PiHat mode!", fg="green")
 
 
 if __name__ == "__main__":
