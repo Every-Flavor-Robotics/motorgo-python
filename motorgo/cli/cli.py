be1fe584838f0faff14bd3f7bdc88be88b1944e9
Swapnil Pande <swapnil.pande98@gmail.com>
2025-01-30 18:19:34 -0500

    Add name to firmware index

--- motorgo/cli/cli.py
+++ motorgo/cli/cli.py
@@ -112,10 +112,11 @@ def flash():
     # Retrieve the firmware index
     firmware_index = requests.get(FIRMWARE_INDEX).json()
     # Retrieve get_board_id_firmware
-    get_board_id_firmware = firmware_index["get_board_id_firmware"]["url"]
+    get_board_id_name = firmware_index["get_board_id_firmware"]["name"]
+    get_board_id_url = firmware_index["get_board_id_firmware"]["url"]
 
     # Download and flash the get_board_id_firmware
-    download_and_flash_firmware("get_board_id", get_board_id_firmware)
+    download_and_flash_firmware(get_board_id_name, get_board_id_url)
 
     board_id = get_board_id()
     if board_id not in board_id_map:
@@ -127,6 +128,9 @@ def flash():
     click.secho("Step 2: Downloading and flashing latest firmware", fg="blue")
     # Retrieve url for the latest firmware
     try:
+        firmware_name = firmware_index["motorgo_python_firmware"][f"0x{board_id:02x}"][
+            "latest"
+        ]["name"]
         firmware_url = firmware_index["motorgo_python_firmware"][f"0x{board_id:02x}"][
             "latest"
         ]["url"]
@@ -134,7 +138,7 @@ def flash():
         click.secho("Error: Firmware not available for this board", fg="red")
         return
 
-    download_and_flash_firmware()
+    download_and_flash_firmware(firmware_name, firmware_url)
 
     click.secho("MotorGo successfully configured for PiHat mode!", fg="green")
 
